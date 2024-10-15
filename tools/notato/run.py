99bcfa231060cc7417becd54c78c3dd2a8ba509e
Scott Underwood <37125095+scottunderwoodomg@users.noreply.github.com>
2024-10-14 20:08:25 -0400

    notato refactor, click support

--- tools/notato/run.py
+++ tools/notato/run.py
@@ -3,6 +3,7 @@ import shutil
 from pathlib import Path
 from datetime import datetime
 from config_loader import get_path
+import click
 
 """
 Improvements Needed
@@ -10,48 +11,93 @@ Improvements Needed
 - TODO: Clean up the MVP code and make a bit more personal
 - TODO: Add click interface for selecting a template but the option to overide and just create a quick note
 
-- TODO: Add the ability to inject date from run.py script (Jinja?)
-- TODO: Capture other system information for a system report note (python version, etc.)
-- TODO: Use Class
+- TODO: Capture other system information for a system report note (python version, etc., team name [derived from directory?])
+- TODO: Consolidate redundant lines of code
 - TODO: Add tags and make an easy "list notes" function for the tool?
 - TODO: Notes Search (ordered by stuff)
 - TODO: Ability to move all of the notes to a single location or interact with them en masse
 - TODO: Add the ablity to open it in your editor of choice
+- TODO: Add docstrings and type hints
+- TODO: Create additional template types from my list in notion: 
+        - Make a bunch of project management/notes/incident report txt file templates
+        - Meeting notes
+        - PRD 
 """
 
 
-sct_home = get_path("sct_home")
-text_editor_path = get_path("text_editor")
-requested_template = "quick_note"
+class NoteCreator:
+    def __init__(self, requested_template, file_name):
+        self.file_name = file_name
+        self.sct_home = get_path("sct_home")
+        self.text_editor_path = get_path("text_editor")
+        self.working_directory = Path.cwd()
+        self.requested_template = requested_template
+        # make method to build file name
+        self.destination_path_string = f"{self.working_directory}/{self.file_name}.md"
 
-working_directory = Path.cwd()
+    def run_creator(self):
+        self.copy_template()
+        self.populate_template(self.set_dynamic_values())
+        self.open_editor()
 
-# Step 1: Define the source and destination paths
-source_path = Path(f"{sct_home}/tools/notato/templates/{requested_template}.md")
-destination_path = Path(f"{working_directory}/template_copy.md")
+    def set_dynamic_values(self):
+        # Step 3: Define dynamic values to replace in the markdown file
+        dynamic_values = {
+            # "{{username}}": "John Doe",
+            "{{ date }}": datetime.now().strftime("%Y-%m-%d"),
+            "{{ time }}": datetime.now().strftime("%Y-%m-%d"),
+            "{{ topic }}": self.file_name,
+        }
 
-# Step 2: Copy the markdown file from source to destination
-shutil.copy(source_path, destination_path)
+        return dynamic_values
 
-# Step 3: Define dynamic values to replace in the markdown file
-dynamic_values = {
-    # "{{username}}": "John Doe",
-    "{{ date }}": datetime.now().strftime("%Y-%m-%d"),
-    "{{ time }}": datetime.now().strftime("%Y-%m-%d"),
-}
+    def copy_template(self):
+        # Step 1: Define the source and destination paths
+        source_path = Path(
+            f"{self.sct_home}/tools/notato/templates/{self.requested_template}.md"
+        )
 
-# Step 4: Read the copied markdown file, replace placeholders, and write changes
-with open(destination_path, "r") as file:
-    content = file.read()
+        # Step 2: Copy the markdown file from source to destination
+        shutil.copy(source_path, Path(self.destination_path_string))
 
-# Step 5: Replace the placeholders with dynamic values
-for placeholder, replacement in dynamic_values.items():
-    content = content.replace(placeholder, replacement)
+    def populate_template(self, dynamic_values):
+        # Step 4: Read the copied markdown file, replace placeholders, and write changes
+        with open(Path(self.destination_path_string), "r") as file:
+            content = file.read()
 
-# Step 6: Write the modified content back to the file
-with open(destination_path, "w") as file:
-    file.write(content)
+        # Step 5: Replace the placeholders with dynamic values
+        for placeholder, replacement in dynamic_values.items():
+            content = content.replace(placeholder, replacement)
 
-# print(f"File copied and modified at: {destination_path}")
-# Open the file you just created in the text editor of your choice
-os.system(f"open -a '{text_editor_path}' '{working_directory}/template_copy.md'")
+        # Step 6: Write the modified content back to the file
+        with open(Path(self.destination_path_string), "w") as file:
+            file.write(content)
+
+    def open_editor(self):
+        # Open the file you just created in the text editor of your choice
+        os.system(
+            f"open -a '{self.text_editor_path}' '{self.working_directory}/{self.file_name}.md'"
+        )
+
+
+@click.command()
+@click.option(
+    "--requested_template",
+    default="quick_note",
+    prompt="What kind of note are you creating?",
+)
+@click.option(
+    "--file_name",
+    default="",
+    prompt="What should the note be called?",
+)
+def run_notato(requested_template: str, file_name: str):
+    """Saves the image from a given Flickr page to the location
+    of your choice
+    """
+    creator = NoteCreator(requested_template=requested_template, file_name=file_name)
+    creator.run_creator()
+
+
+if __name__ == "__main__":
+    run_notato()
---
+++ tools/notato/templates/meeting_note.md
@@ -0,0 +1,15 @@
+# {{ date }} - {{ topic }} Meeting Notes
+
+## Agenda
+1. Item 1
+2. Item 2
+3. Item 3
+
+
+## Meeting Notes
+
+
+
+## Action Items
+- Action Item #1 [assignee]
+- Action Item #2 [assignee]
