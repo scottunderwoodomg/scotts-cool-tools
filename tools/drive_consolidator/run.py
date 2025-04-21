d2fd4395e043969f61a5babbca45c586a72a98e8
Scott Underwood <37125095+scottunderwoodomg@users.noreply.github.com>
2025-04-21 15:32:07 -0400

    file copy and file move support

--- tools/drive_consolidator/run.py
+++ tools/drive_consolidator/run.py
@@ -16,9 +16,7 @@ Create a script that will:
         their respective paths
     - Recursively copy files from the specified source directories into a uniform   
 
-
-Have this code either move files or copy them depending on arg provided
-
+TODO: Add docstrings    
 """
 
 
@@ -30,26 +28,39 @@ class DriveConsolidator:
         self.target_dir = os.path.join(os.getenv("HOME"), target_dir)
         self.retain_source = retain_source
         # TODO: Add ability to ignore certain file type, directory patterns, other
-        # self.ignore_patterns = []
+        self.ignore_patterns = [".DS_Store"]
+        # TODO: Must be a better way to handle the "ignore section cnt"
+        self.ignore_section_cnt = 2
+        self.files_list = self.list_files_walk()
 
     def run_consolidator(self):
         self.common_dir_hierarchy = self.produce_common_dir_hierarchy()
         self.replicate_common_dirs(self.common_dir_hierarchy)
+        self.run_file_transfer()
+
+    def is_ignored(self, root, file):
+        # TODO: Figure out a better way to ignore top-level files
+        if file in self.ignore_patterns or root.count("/") < 1:
+            return True
+        else:
+            return False
 
     def list_files_walk(self):
         file_list = []
         for root, dirs, files in os.walk(self.source_dir):
             for file in files:
-                file_list.append(os.path.join(root, file))
-
+                if not self.is_ignored(root, file):
+                    file_list.append(os.path.join(root, file))
+        print(file_list)
         return file_list
 
-    # TODO: Must be a better way to handle the "ignore section cnt"
-    def produce_common_dir_hierarchy(self, ignore_section_cnt=2):
+    def produce_common_dir_hierarchy(self):
         common_dir_hierarchy = []
-        for file in self.list_files_walk():
+        for file in self.files_list:
             split_path = file.split("/")
-            just_path = "/".join(split_path[ignore_section_cnt : len(split_path) - 1])
+            just_path = "/".join(
+                split_path[self.ignore_section_cnt : len(split_path) - 1]
+            )
 
             if len(just_path) > 0:
                 common_dir_hierarchy.append(just_path)
@@ -68,15 +79,36 @@ class DriveConsolidator:
             path = os.path.join(self.target_dir, directory)
             self.create_path_if_not_exist(path)
 
+    def create_src_tgt_sets(self):
+        return [
+            {
+                "src": file,
+                "tgt": os.path.join(
+                    self.target_dir,
+                    "/".join(file.split("/")[self.ignore_section_cnt :]),
+                ),
+            }
+            for file in self.files_list
+        ]
+
+    def run_file_transfer(self):
+        for file in self.create_src_tgt_sets():
+            if self.retain_source:
+                shutil.copyfile(file["src"], file["tgt"])
+            else:
+                shutil.move(file["src"], file["tgt"])
+
 
 @click.command()
 @click.option(
     "--source_dir",
     default=".",
+    # default=os.getcwd(),
     prompt="What top-level directory are the source files located in?",
 )
 @click.option(
     "--target_dir",
+    # TODO: Replace this default value with a different preferred path?
     default="desktop/path_recreation",
     prompt="What top-level directory should the consolidated copy be created under?",
 )
@@ -86,9 +118,7 @@ class DriveConsolidator:
     prompt="Would you like retain the source as is?",
 )
 def run_drive_consolidator(source_dir: str, target_dir: str, retain_source: bool):
-    """Saves the image from a given Flickr page to the location
-    of your choice
-    """
+    """Add docstring here"""
     consolidator = DriveConsolidator(
         source_dir=source_dir, target_dir=target_dir, retain_source=retain_source
     )
