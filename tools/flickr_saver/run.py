edc67def39995f5290d040777bebf6d6efd1ff24
Scott Underwood <37125095+scottunderwoodomg@users.noreply.github.com>
2025-10-25 09:44:29 -0400

    max version detection bug fix

--- tools/flickr_saver/run.py
+++ tools/flickr_saver/run.py
@@ -77,7 +77,8 @@ class FlickrSaver:
             match = re.match(cleaned_title, filename)
             if match:
                 matched = True
-                last_char = cleaned_title[len(cleaned_title) - 1]
+                filename_ext_removed = filename.split(".")[0]
+                last_char = filename_ext_removed[len(filename_ext_removed) - 1]
                 if last_char.isdigit():
                     current_max = max(current_max, int(last_char))
 
